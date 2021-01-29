8441dd349952480786baba51db30c5a9fe0a677d
Filipe Varjão <varjaofilipe@gmail.com>
2021-01-29 10:22:21 -0300

    [change] sup API call

--- lib/api/sup.ex
+++ lib/api/sup.ex
@@ -3,17 +3,26 @@ defmodule API.Sup do
 
   @url Application.get_env(:audit_kazoo, :base_url)
 
-  @spec exec(atom(), atom(), atom()) :: {:error, any} | {:ok, any}
-  def exec(module, function, args) do
-    # current not working
+  @spec apply(atom() | String.t(), atom() | String.t(), atom() | String.t() | list()) ::
+          {:error, any} | {:ok, any}
+  def apply(module, function \\ "", args \\ []) do
+    # NOTE all modules will become a {MODULE}_maintenance
     auth_token = Utils.get_auth_token()
     header = ["X-Auth-Token": auth_token]
 
-    "#{@url}:8000/v2/sup/#{module}/#{function}/#{args}"
+    "#{@url}:8000/v2/sup"
+    |> build_url(module)
+    |> build_url(function)
+    |> build_url(args)
     |> HTTPoison.get(header)
     |> case do
       {:ok, %{body: body, status_code: 200}} -> Utils.decode(body)
       {:error, %{reason: reason}} -> {:error, reason}
     end
   end
+
+  defp build_url(url, ""), do: url
+  defp build_url(url, []), do: url
+  defp build_url(url, [arg1 | args]), do: build_url("#{url}/#{arg1}", args)
+  defp build_url(url, param), do: "#{url}/#{param}"
 end
