201b6248b868ea7245e8aaca511b6e6972963af9
Filipe Varjão <varjaofilipe@gmail.com>
2021-02-01 17:33:42 -0300

    [add] more actions with qubicle recipient

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ This repo's main goal is to enable developers to run some integration tests for 
 If you want to write tests for the [monster-UI](https://github.com/2600hz/monster-ui), take a look at the [wallaby](https://github.com/elixir-wallaby/wallaby) documentation, it shows what you can do with the library.
 
 This repo also provides a client consumer for Kazoo's API.
+
+We might use Google Chrome, Mozilla Firefox, or selenium to run the browser tests. By default, we are using google chrome
+which requires the [ChromeDriver](https://chromedriver.chromium.org/downloads) installation with the same version of your local browser.
--- lib/api/qubicle_recipient.ex
+++ lib/api/qubicle_recipient.ex
@@ -29,4 +29,73 @@ defmodule API.QubicleRecipient do
       {:error, %{reason: reason}} -> {:error, reason}
     end
   end
+
+  @spec get_recipient_list_status(list()) :: {:error, any} | {:ok, any}
+  def get_recipient_list_status(recipient_ids) do
+    auth_token = Utils.get_auth_token()
+    header = ["X-Auth-Token": auth_token, "Content-Type": :"application/json"]
+    body = Poison.encode!(%{data: %{recipient_ids: recipient_ids}})
+
+    "#{@url}:8000/v2/accounts/#{@account_id}/qubicle_recipients/status"
+    |> HTTPoison.post(body, header)
+    |> case do
+      {:ok, %{body: body, status_code: 200}} -> Utils.decode(body)
+      {:error, %{reason: reason}} -> {:error, reason}
+    end
+  end
+
+  @spec login_recipient(String.t()) :: {:error, any} | {:ok, any}
+  def login_recipient(recipient_id), do: recipient_action(recipient_id, :login)
+
+  @spec logout_recipient(String.t()) :: {:error, any} | {:ok, any}
+  def logout_recipient(recipient_id), do: recipient_action(recipient_id, :logout)
+
+  @spec wrapup_extend(String.t()) :: {:error, any} | {:ok, any}
+  def wrapup_extend(recipient_id), do: recipient_action(recipient_id, :wrapup_extend)
+
+  @spec wrapup_cancel(String.t()) :: {:error, any} | {:ok, any}
+  def wrapup_cancel(recipient_id), do: recipient_action(recipient_id, :wrapup_cancel)
+
+  @spec set_recipient_status(String.t(), :login | :logout | :ready | :away) ::
+          {:error, any} | {:ok, any}
+  def set_recipient_status(recipient_id, status) do
+    auth_token = Utils.get_auth_token()
+    header = ["X-Auth-Token": auth_token, "Content-Type": :"application/json"]
+    body = Poison.encode!(%{data: %{status: status}})
+
+    "#{@url}:8000/v2/accounts/#{@account_id}/qubicle_recipients/#{recipient_id}/status"
+    |> HTTPoison.post(body, header)
+    |> case do
+      {:ok, %{body: body, status_code: 200}} -> Utils.decode(body)
+      {:error, %{reason: reason}} -> {:error, reason}
+    end
+  end
+
+  @spec monitor_recipient(String.t(), :eavesdrop | :whisper | :barge) ::
+          {:error, any} | {:ok, any}
+  def monitor_recipient(recipient_id, action) do
+    auth_token = Utils.get_auth_token()
+    header = ["X-Auth-Token": auth_token, "Content-Type": :"application/json"]
+    body = Poison.encode!(%{data: %{action: :monitor, target: recipient_id, mode: action}})
+
+    "#{@url}:8000/v2/accounts/#{@account_id}/qubicle_recipients/#{recipient_id}"
+    |> HTTPoison.post(body, header)
+    |> case do
+      {:ok, %{body: body, status_code: 200}} -> Utils.decode(body)
+      {:error, %{reason: reason}} -> {:error, reason}
+    end
+  end
+
+  defp recipient_action(recipient_id, action) do
+    auth_token = Utils.get_auth_token()
+    header = ["X-Auth-Token": auth_token, "Content-Type": :"application/json"]
+    body = Poison.encode!(%{data: %{action: action}})
+
+    "#{@url}:8000/v2/accounts/#{@account_id}/qubicle_recipients/#{recipient_id}"
+    |> HTTPoison.post(body, header)
+    |> case do
+      {:ok, %{body: body, status_code: 200}} -> Utils.decode(body)
+      {:error, %{reason: reason}} -> {:error, reason}
+    end
+  end
 end
