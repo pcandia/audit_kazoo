4dfb76d15bd9535ed88e8526ae3a5db0cd2e2f4e
Filipe Varjão <varjaofilipe@gmail.com>
2021-02-26 14:42:48 -0300

    [add] api consumer for callflow's endpoints

---
+++ lib/api/callflows.ex
@@ -0,0 +1,88 @@
+defmodule API.Callflows do
+  alias API.Utils
+
+  @url Application.get_env(:audit_kazoo, :base_url)
+  @account_id Application.get_env(:audit_kazoo, :account_id)
+
+  @spec fetch_callflows() :: {:error, any} | {:ok, any}
+  def fetch_callflows do
+    auth_token = Utils.get_auth_token()
+    header = ["X-Auth-Token": auth_token]
+
+    "#{@url}:8000/v2/accounts/#{@account_id}/callflows"
+    |> HTTPoison.get(header)
+    |> case do
+      {:ok, %{body: body, status_code: 200}} -> Utils.decode(body)
+      {:error, %{reason: reason}} -> {:error, reason}
+    end
+  end
+
+  @spec fetch_callflow(String.t()) :: {:error, any} | {:ok, any}
+  def fetch_callflow(callflow_id) do
+    auth_token = Utils.get_auth_token()
+    header = ["X-Auth-Token": auth_token]
+
+    "#{@url}:8000/v2/accounts/#{@account_id}/callflows/#{callflow_id}"
+    |> HTTPoison.get(header)
+    |> case do
+      {:ok, %{body: body, status_code: 200}} -> Utils.decode(body)
+      {:error, %{reason: reason}} -> {:error, reason}
+    end
+  end
+
+  @spec create_callflow(%{name: String.t(), numbers: list(), patterns: list()}) ::
+          {:error, any} | {:ok, any}
+  def create_callflow(data) do
+    auth_token = Utils.get_auth_token()
+    header = ["X-Auth-Token": auth_token, "Content-Type": :"application/json"]
+    body = Poison.encode!(%{data: data})
+
+    "#{@url}:8000/v2/accounts/#{@account_id}/callflows"
+    |> HTTPoison.put(body, header)
+    |> case do
+      {:ok, %{body: body, status_code: 201}} -> Utils.decode(body)
+      {:error, %{reason: reason}} -> {:error, reason}
+    end
+  end
+
+  @spec delete_callflow(String.t()) :: {:error, any} | {:ok, any}
+  def delete_callflow(callflow_id) do
+    auth_token = Utils.get_auth_token()
+    header = ["X-Auth-Token": auth_token]
+
+    "#{@url}:8000/v2/accounts/#{@account_id}/callflows/#{callflow_id}"
+    |> HTTPoison.delete(header)
+    |> case do
+      {:ok, %{body: body, status_code: 200}} -> Utils.decode(body)
+      {:error, %{reason: reason}} -> {:error, reason}
+    end
+  end
+
+  @spec patch_callflow(String.t(), map()) :: {:error, any} | {:ok, any}
+  def patch_callflow(callflow_id, data) do
+    auth_token = Utils.get_auth_token()
+    header = ["X-Auth-Token": auth_token, "Content-Type": :"application/json"]
+    body = Poison.encode!(%{data: data})
+
+    "#{@url}:8000/v2/accounts/#{@account_id}/callflows/#{callflow_id}"
+    |> HTTPoison.patch(body, header)
+    |> case do
+      {:ok, %{body: body, status_code: 200}} -> Utils.decode(body)
+      {:error, %{reason: reason}} -> {:error, reason}
+    end
+  end
+
+  @spec change_callflow(String.t(), map()) :: {:error, any} | {:ok, any}
+  def change_callflow(callflow_id, data) do
+    auth_token = Utils.get_auth_token()
+    header = ["X-Auth-Token": auth_token, "Content-Type": :"application/json"]
+    body = Poison.encode!(%{data: data})
+
+    "#{@url}:8000/v2/accounts/#{@account_id}/callflows/#{callflow_id}"
+    |> HTTPoison.post(body, header)
+    |> case do
+      {:ok, %{body: body, status_code: 200}} -> Utils.decode(body)
+      {:error, %{reason: reason}} -> {:error, reason}
+    end
+  end
+end
