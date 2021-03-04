ff40bbcd92b296e296a0aa5c7020e418f5d3e42b
Filipe Varjão <varjaofilipe@gmail.com>
2021-03-04 12:14:04 -0300

    adding libwebphone to gitignore and using embedded elixir EEx

--- lib/audit_kazoo/router.ex
+++ lib/audit_kazoo/router.ex
@@ -2,8 +2,18 @@ defmodule AuditKazoo.Router do
   use Plug.Router
   use Plug.Debugger, otp_app: :audit_kazoo
 
+  require EEx
+
+  plug Plug.Static, at: "/dist", from: "libwebphone/dist/"
   plug(:match)
   plug(:dispatch)
+
+  @url Application.get_env(:audit_kazoo, :base_url)
+  @sip_caller_username Application.get_env(:audit_kazoo, :sip_caller_username)
+  @sip_caller_password Application.get_env(:audit_kazoo, :sip_caller_password)
+  @sip_callee_username Application.get_env(:audit_kazoo, :sip_callee_username)
+  @sip_callee_password Application.get_env(:audit_kazoo, :sip_callee_password)
+  @realm Application.get_env(:audit_kazoo, :realm, "realm.com")
 
   get "" do
     localhost_ip =
@@ -12,7 +22,15 @@ defmodule AuditKazoo.Router do
       |> to_string()
 
     Application.put_env(:audit_kazoo, :webhook_uri, localhost_ip)
-    send_resp(conn, 200, "Hello!")
+    page_contents = EEx.eval_file("libwebphone/redphone.eex", [base_url: @url, sip_username: @sip_caller_username, sip_password: @sip_caller_password, realm: @realm])
+    conn = Plug.Conn.put_resp_content_type(conn, "text/html")
+    send_resp(conn, 200, page_contents)
+  end
+
+  get "/telephone" do
+    page_contents = EEx.eval_file("libwebphone/telephone.eex", [base_url: @url, sip_username: @sip_callee_username, sip_password: @sip_callee_password, realm: @realm])
+    conn = Plug.Conn.put_resp_content_type(conn, "text/html")
+    send_resp(conn, 200, page_contents)
   end
 
   post "/events" do
